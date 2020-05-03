7c474a2b9f7f06f412cad65a09b5305de6ed5f27
PrasheethaChouhan <60392154+PrasheethaChouhan@users.noreply.github.com>
2020-05-03 10:06:46 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,98 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(200,100);
+    car2 = createSprite(200,300);
+    car3 = createSprite(200,500);
+    car4 = createSprite(200,700);
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+    
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      background(rgb(198,135,103));
+      //image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      
+      //var display_position = 100;
+      
+      //index of the array
+      var index = 0;
+
+      //x and y position of the cars
+      var y = 175 ;
+      var x;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        y = y + 200;
+        //use data form the database to display the cars in y direction
+        x = displayWidth - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+       // console.log(index, player.index)
+
+       
+        if (index === player.index){
+          
+          cars[index - 1].shapeColor = "red";
+          camera.position.y = displayHeight/2;
+          camera.position.x = cars[index-1].x;
+        }
+       
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(RIGHT_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    if(player.distance > 3860){
+      gameState = 2;
+    }
+   
+    drawSprites();
+  }
+
+  end(){
+    console.log("Game Ended");
+  }
+}
